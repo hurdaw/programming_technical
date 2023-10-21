9051c0ad3c27f0421abaf139e1442bc42cdfc187
hurdaw <tn133931@gmail.com>
2023-10-21 14:42:21 +0700

    2110

---
+++ graph/BFS.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+vector<int> adj[100];
+bool visited[100];
+// 10 11
+// 1 2
+// 1 3
+// 1 5
+// 1 10
+// 2 4
+// 3 6
+// 3 7
+// 3 9
+// 6 7
+// 5 8
+// 8 9
+
+void inp()
+{
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    memset(visited, false, sizeof(visited));
+}
+void BFS(int u)
+{
+    queue<int> q;
+    q.push(u);
+    visited[u] = true;
+    while (!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+        cout << v << " ";
+        for (int x : adj[v])
+        {
+            if (!visited[x])
+            {
+                q.push(x);
+                visited[x] = true;
+            }
+        }
+    }
+}
+int main()
+{
+    inp();
+    BFS(1);
+}
--- graph/DFS.cpp
+++ graph/DFS.cpp
@@ -5,20 +5,28 @@ vector<int> adj[100];
 int a[100][100];
 bool visited[100], check[100];
 // input
-// 9 9
+// 13 16 
 // 1 2
 // 1 3
-// 1 5
-// 2 4
-// 3 6
-// 3 7
-// 3 9
-// 5 8
-// 8 9
+// 1 11
+// 2 
+// 4
+// 2 6
+// 2 7
+// 3 4
+// 4 6
+// 5 9
+// 5 10
+// 6 7
+// 6 8
+// 8 10
+// 9 13
+// 11 12
+// 11 13
 void input_dsk()
 {
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < m; i++)
     {
         int x, y;
         cin >> x >> y;
@@ -65,19 +73,9 @@ void DFS_mtk(int u)
 int main()
 {
     input_dsk();
-    input_mtk();
-    clock_t start, end;
-    start = clock();
+    //input_mtk();
+
     DFS(1);
-    end = clock();
-    double time_used_1 = (double)(end - start) / CLOCKS_PER_SEC;
-    cout << endl
-         << time_used_1;
     cout << endl;
-    start = clock();
-    DFS_mtk(1);
-    end = clock();
-    double time_used_2 = (double)(end - start) / CLOCKS_PER_SEC;
-    cout << endl
-         << time_used_2;
+    //DFS_mtk(1);
 }
